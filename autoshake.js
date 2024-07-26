39f7b441abcea02d559675197889a89cc58aca7e
nostopgmaming17 <noamshukrun2008@gmail.com>
2024-07-26 21:15:32 +0300

    Create autoshake.js

---
+++ autoshake.js
@@ -0,0 +1,40 @@
+plugin.pluginData = {
+    name: "Auto shake",
+    description: "Emulate mouse shake when holding shake keybind",
+    author: "noam"
+};
+
+plugin.registerKeybind("shake", "KeyV");
+
+const formMovePacket = function (x, y, idk) {
+    const buf = new ArrayBuffer(10);
+    const d = new DataView(buf);
+    d.setUint8(0, 2);
+    d.setInt32(1, x, true);
+    d.setInt32(5, y, true);
+    d.setUint8(9, idk);
+    return buf;
+}
+
+let FORWARD = false;
+
+let game;
+blockyfish.addEventListener("gameInit", ({ game: _game }) => {
+    game = _game;
+});
+
+setInterval(() => {
+    if (game == null) return;
+    if (!plugin.isKeybindDown("shake")) return;
+    if (game.currentScene == null || game.currentScene.myAnimal == null || game.inputManager == null || game.inputManager.getMouseWorldPosition == null) return;
+    let mpos = game.inputManager.getMouseWorldPosition();
+    let ppos = game.currentScene.myAnimal.position;
+    let angleRadians = Math.atan2(mpos.y - ppos._y, mpos.x - ppos._x);
+    let incr = FORWARD ? 1000 : 750;
+    let bpos = [
+        ppos._x + (Math.cos(angleRadians) * incr),
+        ppos._y + (Math.sin(angleRadians) * incr)
+    ];
+    game.socketManager.sendBytePacket(formMovePacket(bpos[0], bpos[1], 100));
+    FORWARD = !FORWARD;
+}, 100);
