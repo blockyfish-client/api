3a9c8d42ef5db80377ea5a10eb77bf3502392e2e
nostopgmaming17 <noamshukrun2008@gmail.com>
2024-07-26 21:06:40 +0300

    Create iseedeadfish.js

---
+++ iseedeadfish.js
@@ -0,0 +1,9 @@
+plugin.pluginData = {
+    name: "I see dead fish!",
+    author: "pi & noam",
+    description: "Lets you see ghosts"
+};
+
+blockyfish.addEventListener("gameInit", ({ game }) => {
+    game.currentScene.viewingGhosts = true;
+});
