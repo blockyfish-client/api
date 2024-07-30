e72f0d99d58deb31c921563c089325a77f6f337f
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-07-29 21:21:46 -0700

    clean up

--- plugins/bettervision.js
+++ plugins/bettervision.js
@@ -1,11 +1,11 @@
 plugin.pluginData = {
-    name: "Better vision",
-    description: "No ink flash or deep darkness effects",
-    author: "pi & noam"
+	name: "Better vision",
+	description: "No ink flash or deep darkness effects",
+	author: "pi & noam",
 };
 
 blockyfish.addEventListener("gameInit", ({ game }) => {
-    game.currentScene.toggleFlash = function () { };
-    game.currentScene.terrainManager.shadow.setShadowSize(1000000);
-    game.currentScene.terrainManager.shadow.setShadowSize = function () { };
+	game.currentScene.toggleFlash = () => {};
+	game.currentScene.terrainManager.shadow.setShadowSize(1000000);
+	game.currentScene.terrainManager.shadow.setShadowSize = () => {};
 });
--- plugins/xray.js
+++ plugins/xray.js
@@ -1,22 +1,22 @@
 plugin.pluginData = {
-    name: "XRAY",
-    description: "See underground caves and animals through terrain",
-    author: "pi & noam"
+	name: "XRAY",
+	description: "See underground caves and animals through terrain",
+	author: "pi & noam",
 };
 
 let game;
-setInterval(function () {
-    try {
-        if (game == null || game.currentScene == null) return;
-        game.currentScene.ceilingsContainer.alpha = 0.3;
-    } catch { }
+setInterval(() => {
+	try {
+		if (game == null || game.currentScene == null) return;
+		game.currentScene.ceilingsContainer.alpha = 0.3;
+	} catch {}
 }, 200);
 blockyfish.addEventListener("gameInit", ({ game: _game }) => {
-    game = _game;
-    game.currentScene.foodGlowContainer.zOrder = 996;
-    game.currentScene.foodContainer.zOrder = 997;
-    game.currentScene.namesLayer.zOrder = 998;
-    game.currentScene.animalsContainer.zOrder = 999;
-    game.currentScene.barsLayer.zOrder = 1000;
-    game.currentScene.chatContainer.zOrder = 1001;
+	game = _game;
+	game.currentScene.foodGlowContainer.zOrder = 996;
+	game.currentScene.foodContainer.zOrder = 997;
+	game.currentScene.namesLayer.zOrder = 998;
+	game.currentScene.animalsContainer.zOrder = 999;
+	game.currentScene.barsLayer.zOrder = 1000;
+	game.currentScene.chatContainer.zOrder = 1001;
 });
