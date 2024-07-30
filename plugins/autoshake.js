5c2044620130970fce511180d6dd61239b26b39a
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-07-29 21:30:33 -0700

    minor performance improvement

--- plugins/autoshake.js
+++ plugins/autoshake.js
@@ -23,24 +23,32 @@ blockyfish.addEventListener("gameInit", ({ game: _game }) => {
 	game = _game;
 });
 
-setInterval(() => {
-	if (game == null) return;
-	if (!plugin.isKeybindDown("shake")) return;
-	if (
-		game.currentScene == null ||
-		game.currentScene.myAnimal == null ||
-		game.inputManager == null ||
-		game.inputManager.getMouseWorldPosition == null
-	)
-		return;
-	const mpos = game.inputManager.getMouseWorldPosition();
-	const ppos = game.currentScene.myAnimal.position;
-	const angleRadians = Math.atan2(mpos.y - ppos._y, mpos.x - ppos._x);
-	const incr = FORWARD ? 1000 : 750;
-	const bpos = [
-		ppos._x + Math.cos(angleRadians) * incr,
-		ppos._y + Math.sin(angleRadians) * incr,
-	];
-	game.socketManager.sendBytePacket(formMovePacket(bpos[0], bpos[1], 100));
-	FORWARD = !FORWARD;
-}, 100);
+let inter = null;
+plugin.onKeybindDown("shake", () => {
+	if (inter != null) return;
+	inter = setInterval(() => {
+		if (game == null) return;
+		if (!plugin.isKeybindDown("shake")) return;
+		if (
+			game.currentScene == null ||
+			game.currentScene.myAnimal == null ||
+			game.inputManager == null ||
+			game.inputManager.getMouseWorldPosition == null
+		)
+			return;
+		const mpos = game.inputManager.getMouseWorldPosition();
+		const ppos = game.currentScene.myAnimal.position;
+		const angleRadians = Math.atan2(mpos.y - ppos._y, mpos.x - ppos._x);
+		const incr = FORWARD ? 1000 : 750;
+		const bpos = [
+			ppos._x + Math.cos(angleRadians) * incr,
+			ppos._y + Math.sin(angleRadians) * incr,
+		];
+		game.socketManager.sendBytePacket(formMovePacket(bpos[0], bpos[1], 100));
+		FORWARD = !FORWARD;
+	}, 100);
+});
+plugin.onKeybindUp("shake", () => {
+	clearInterval(inter);
+	inter = null;
+});
