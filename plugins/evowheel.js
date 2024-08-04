445da7f3eabb5cd1a6b842f26d81bba220a4149e
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-08-04 05:13:16 -0700

    Create evowheel.js

---
+++ plugins/evowheel.js
@@ -0,0 +1,66 @@
+// @name Evolution tree wheel
+// @id evowheel
+// @description View an evolution tree for tiers 8 - 10
+// @author pi
+
+plugin.registerKeybind("Show evo wheel", "KeyY");
+
+const evowheelCss = document.createElement("style");
+evowheelCss.innerHTML = `
+@keyframes evowheel-spin {
+    from {
+        rotate: 0deg;
+    }
+    to {
+        rotate: 360deg;
+    }
+}
+.evowheel {
+    animation: evowheel-spin 54s linear infinite;
+    transition: all 0.3s ease-out;
+    opacity: 0;
+    transform: scale(0) rotate(-90deg);
+
+    max-width: 80vw;
+    max-height: 80vh;
+    aspect-ratio: 1;
+    z-index: 9999;
+    position: fixed;
+    top: 50%;
+    left: 50%;
+    translate: -50% -50%;
+    pointer-events: none;
+}
+.evowheel.open {
+    opacity: 1;
+    transform: scale(1) rotate(0deg);
+}`;
+document.querySelector("head").append(evowheelCss);
+
+const evowheelUrl = "https://blockyfish-client.github.io/assets/evo_circle.png";
+const createEvowheel = () => {
+	const evowheel = document.createElement("div");
+	document.querySelector("div.game").prepend(evowheel);
+	ReactDOM.createRoot(evowheel).render(
+		<img src={evowheelUrl} draggable="false" className="evowheel" alt="" />,
+	);
+};
+
+blockyfish.addEventListener("gameInit", ({ game: _game }) => {
+	createEvowheel();
+});
+
+let isOpen = false;
+plugin.onKeybindDown("Show evo wheel", () => {
+	if (isOpen) return;
+	for (const evowheel of document.querySelectorAll(".evowheel")) {
+		evowheel.classList.add("open");
+	}
+	isOpen = true;
+});
+plugin.onKeybindUp("Show evo wheel", () => {
+	for (const evowheel of document.querySelectorAll(".evowheel")) {
+		evowheel.classList.remove("open");
+	}
+	isOpen = false;
+});
