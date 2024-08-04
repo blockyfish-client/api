c330e78dac4a791fc84bc53e4c967c6ee27490b6
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-08-04 04:23:00 -0700

    Create autospin.js

---
+++ plugins/autospin.js
@@ -0,0 +1,42 @@
+// @name Auto spin
+// @id autospin
+// @description Spin really fast without breaking your arms
+// @author pi
+
+plugin.registerKeybind("spin", "KeyZ");
+
+let mapeditor;
+blockyfish.addEventListener("gameInit", () => {
+	mapeditor = document.querySelector("#canvas-container > canvas");
+});
+
+let spin_direction = 0;
+const spin_angle = [0, 30, 60, 90, 120, 150, 180, 210, 240, 270, 300, 330, 360];
+const spin_radius = 300;
+
+let inter = null;
+plugin.onKeybindDown("spin", () => {
+	if (inter != null) return;
+	inter = setInterval(() => {
+		if (!plugin.isKeybindDown("spin")) return;
+		const spin_coords_x = Math.round(
+			spin_radius * Math.sin((Math.PI * 2 * spin_angle[spin_direction]) / 360),
+		);
+		const spin_coords_y = Math.round(
+			spin_radius * Math.cos((Math.PI * 2 * spin_angle[spin_direction]) / 360),
+		);
+		try {
+			mapeditor?.dispatchEvent(
+				new MouseEvent("pointermove", {
+					clientX: innerWidth / 2 + spin_coords_x,
+					clientY: innerHeight / 2 + spin_coords_y,
+				}),
+			);
+			spin_direction = (spin_direction + 1) % 11;
+		} catch {}
+	}, 15);
+});
+plugin.onKeybindUp("spin", () => {
+	clearInterval(inter);
+	inter = null;
+});
