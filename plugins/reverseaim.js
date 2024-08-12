f6b61daa9825a219b2d8ed73a6511d98107ad5fe
nostopgmaming17 <noamshukrun2008@gmail.com>
2024-08-12 11:09:03 +0300

    added reverseaim for thresher and new stringray

---
+++ plugins/reverseaim.js
@@ -0,0 +1,33 @@
+// @name Reverse aim
+// @id reverseaim
+// @description Automatically reverses movement 
+// @author noam
+
+let game;
+let reverse = false;
+blockyfish.addEventListener("gameInit", ({ game: _game }) => {
+    game = _game;
+    if (getnative(game.inputManager.getMouseWorldPosition)) {
+        hook(game.inputManager, game.inputManager.key_getMouseWorldPosition, {
+            apply(f, th, args) {
+                if (!reverse || game == null || game.currentScene == null || game.currentScene.myAnimal == null) return reflect.apply(f, th, args);
+                const pos = reflect.apply(f, th, args);
+                const mypos = game.currentScene.getMyPos();
+                pos.x = -(pos.x - mypos._x) + mypos._x;
+                pos.y = -(pos.y - mypos._y) + mypos._y;
+                return pos;
+            }
+        });
+    }
+});
+
+setInterval(() => {
+    if (game == null || game.currentScene == null || game.currentScene.myAnimal == null) return;
+    const animal = game.currentScene.myAnimal;
+    switch (animal.fishLevelData.fishLevel) {
+        case 101:
+        case 126:
+            reverse = animal._usingSkill
+            break;
+    }
+});
