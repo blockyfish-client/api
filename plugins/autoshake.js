66323af0050e33e81a169461d7a8994c25fb896c
Pi <90981829+ItsPi3141@users.noreply.github.com>
2024-08-19 11:38:49 -0700

    make autoshake toggle

--- plugins/autoshake.js
+++ plugins/autoshake.js
@@ -1,6 +1,6 @@
 // @name Auto shake
 // @id autoshake
-// @description Emulate mouse shake when holding shake keybind
+// @description Press the shake keybind to toggle
 // @author noam
 
 plugin.registerKeybind("Shake", "KeyV");
@@ -22,31 +22,47 @@ blockyfish.addEventListener("gameInit", ({ game: _game }) => {
 	game = _game;
 });
 
+let held = false;
 let inter = null;
 plugin.onKeybindDown("Shake", () => {
-	if (inter != null) return;
-	inter = setInterval(() => {
-		if (game == null) return;
-		if (
-			game.currentScene == null ||
-			game.currentScene.myAnimal == null ||
-			game.inputManager == null ||
-			game.inputManager.getMouseWorldPosition == null
-		)
-			return;
-		const mpos = game.inputManager.getMouseWorldPosition();
-		const ppos = game.currentScene.myAnimal.position;
-		const angleRadians = Math.atan2(mpos.y - ppos._y, mpos.x - ppos._x);
-		const incr = FORWARD ? 1000 : 750;
-		const bpos = [
-			ppos._x + Math.cos(angleRadians) * incr,
-			ppos._y + Math.sin(angleRadians) * incr,
-		];
-		game.socketManager.sendBytePacket(formMovePacket(bpos[0], bpos[1], 100));
-		FORWARD = !FORWARD;
-	}, 100);
+	if (held) return;
+	held = true;
+	if (inter != null) {
+		clearInterval(inter);
+		inter = null;
+		game.currentScene.showMessagePopup(
+			"Autoshake has been disabled",
+			1000,
+			false,
+		);
+	} else {
+		inter = setInterval(() => {
+			if (game == null) return;
+			if (
+				game.currentScene == null ||
+				game.currentScene.myAnimal == null ||
+				game.inputManager == null ||
+				game.inputManager.getMouseWorldPosition == null
+			)
+				return;
+			const mpos = game.inputManager.getMouseWorldPosition();
+			const ppos = game.currentScene.myAnimal.position;
+			const angleRadians = Math.atan2(mpos.y - ppos._y, mpos.x - ppos._x);
+			const incr = FORWARD ? 2000 : 1500;
+			const bpos = [
+				ppos._x + Math.cos(angleRadians) * incr,
+				ppos._y + Math.sin(angleRadians) * incr,
+			];
+			game.socketManager.sendBytePacket(formMovePacket(bpos[0], bpos[1], 100));
+			FORWARD = !FORWARD;
+		}, 100);
+		game.currentScene.showMessagePopup(
+			"Autoshake has been enabled",
+			1000,
+			false,
+		);
+	}
 });
 plugin.onKeybindUp("Shake", () => {
-	clearInterval(inter);
-	inter = null;
+	held = false;
 });
