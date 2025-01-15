2081200746d17b4c7693448324598f2952b212c2
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-01-15 17:48:06 +0200

    added fiddler plugin

---
+++ plugins/fiddler.js
@@ -0,0 +1,46 @@
+// @name Fiddler godmode
+// @id fiddlergodmodeop
+// @description Press toggle godmode keybind to get godmode
+// @author noam
+
+plugin.registerSetting({
+	name: "Delay (MS)",
+	description: "How long should delay between heals be (recommended 100)",
+	type: "number",
+	defaultValue: 100
+});
+plugin.registerSetting({
+	name: "Limit",
+	description: "At what amount of overhealth does it stop healing (Max over health is 3333, put over 3333 for no limit)",
+	type: "number",
+	defaultValue: 3333
+});
+plugin.registerKeybind("Toggle Godmode", "KeyQ")
+
+let game;
+let toggle = false;
+plugin.onKeybindDown("Toggle Godmode", () => {
+	if (game?.currentScene?.myAnimal != null && game.currentScene.myAnimal.fishLevelData.fishLevel === 121)
+		toggle = !toggle;
+});
+
+blockyfish.addEventListener("gameInit", ({ game: _game }) => {
+	game = _game;
+});
+let delay = plugin.getSetting("Delay (MS)");
+let interval;
+function tick() {
+	try {
+		if (plugin.getSetting("Delay (MS)") != delay && interval != null) {
+			clearInterval(interval);
+			delay = plugin.getSetting("Delay (MS)");
+			interval = setInterval(tick, delay);
+		}
+		if (game?.currentScene?.myAnimal?.fishLevelData?.fishLevel !== 121)
+			return toggle = false;
+		if (game.currentScene.myAnimal.fishData.overHealth >= plugin.getSetting("Limit")) return;
+		if (!toggle) return;
+		game.socketManager.sendBytePacket(blockyfish.encodeBytePacket(gameState.token._value, blockyfish.ActionPacket.FiddlerScore));
+	} catch { }
+}
+interval = setInterval(tick, delay);
