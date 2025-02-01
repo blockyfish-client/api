71f9fe6c49c4e6f719fc22278a4b26adabb7012c
nostopgmaming17 <noamshukrun2008@gmail.com>
2025-02-01 18:53:24 +0200

    add custombackground plugin

---
+++ plugins/custombackground.js
@@ -0,0 +1,28 @@
+// @name Custom background
+// @id custombackground
+// @description Lets you have a custom background image/gif
+// @author noam
+
+plugin.registerSetting({
+	name: "URL",
+	type: "string",
+	description: "URL to custom background image/gif",
+	defaultValue: ""
+});
+
+const url = plugin.getSetting("URL").trim();
+if (url != "") {
+    let homebg = document.querySelector(".home-bg")
+    const onhomebg = () => {
+        homebg.style.setProperty("background-image", `url("${url}")`, "important");
+    };
+    if (homebg == null) {
+        let interval = setInterval(()=>{
+            homebg = document.querySelector(".home-bg")
+            if (homebg != null) clearInterval(interval);
+            onhomebg();
+        })
+    } else {
+        onhomebg();
+    }
+}
