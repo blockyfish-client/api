97399dafc03958141e31e9857f34fb4cc11e8eef
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-03-09 16:16:42 -0700

    feat: simplify registry data

--- deploy.js
+++ deploy.js
@@ -12,6 +12,13 @@ const { default: remarkRehype } = require("remark-rehype");
 const { default: rehypeSanitize } = require("rehype-sanitize");
 const { default: rehypeStringify } = require("rehype-stringify");
 
+// load registry
+const reg = JSON.parse(
+	fs.readFileSync("registry.json", {
+		encoding: "utf8",
+	}),
+);
+
 // compile announcements
 const compileMarkdown = (content) => {
 	return unified()
@@ -26,18 +33,11 @@ const announcements = compileMarkdown(
 	fs.readFileSync("announcements.md", { encoding: "utf8" }),
 ).value;
 
-// edit registry
-const reg = JSON.parse(
-	fs.readFileSync("registry.json", {
-		encoding: "utf8",
-	}),
-);
+// add announcements
 reg.announcements = announcements;
 
-// minify registry
-fs.writeFileSync("registry.json", JSON.stringify(reg));
-
-// minify plugins
+// minify plugins and write metadata to registry
+reg.plugins = [];
 const compileJsx = (content) => {
 	try {
 		const { code } = transformSync(content, {
@@ -72,4 +72,21 @@ for (const plugin of fs.readdirSync("plugins")) {
 		},
 	}).code;
 	fs.writeFileSync(`plugins/${plugin}`, `${info}\n${minified}`);
+
+	// parse metadata
+	const matches = info.matchAll(/\/\/ @(?<field>.+?) (?<value>.+)$/gm);
+	const metadata = {};
+	for (const match of matches) {
+		metadata[match.groups.field] = match.groups.value;
+	}
+	for (const data in reg.pluginsMetadata[metadata.id]) {
+		metadata[data] = reg.pluginsMetadata[metadata.id][data];
+	}
+	reg.plugins.push(metadata);
 }
+
+// remove unused pluginMetadata
+reg.pluginsMetadata = undefined;
+
+// minify and save registry
+fs.writeFileSync("registry.json", JSON.stringify(reg));
