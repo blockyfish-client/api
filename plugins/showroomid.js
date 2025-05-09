dbfdee596911038c3a1e3280538ad08ecbbac31e
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2025-05-09 14:02:11 -0700

    feat: add show room ID plugin

---
+++ plugins/showroomid.js
@@ -0,0 +1,33 @@
+// @name Show room ID
+// @id showroomid
+// @description Display everyone's room ID next to their name
+// @author pi
+// @tags visual, gameplay, client-side
+
+const tag = `id-${Math.random().toString(36).slice(2, 6)}`;
+const style = {
+	fill: "#bfbfbf",
+	fontSize: 30,
+	fontWeight: "bold",
+};
+
+let game;
+blockyfish.addEventListener("gameInit", ({ game: _game }) => {
+	game = _game;
+});
+
+setInterval(() => {
+	try {
+		if (game == null || game.currentScene == null) return;
+		for (
+			let i = 0;
+			i < game.currentScene.entityManager.animalsList.length;
+			i++
+		) {
+			const animal = game.currentScene.entityManager.animalsList[i];
+			animal.nameObject.textStyles[tag] = style;
+
+			animal.nameObject.text = `${animal.entityName} <${tag}>[${animal.playerRoomId}]</${tag}>`;
+		}
+	} catch {}
+}, 200);
