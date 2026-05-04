1926678dbc09a63fb445b1214b2c40b878b832a9
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2026-05-04 12:05:58 -0700

    fix: override toggleFlash

--- plugins/bettervision.js
+++ plugins/bettervision.js
@@ -5,7 +5,8 @@
 // @tags visual, gameplay, client-side
 
 blockyfish.addEventListener("gameInit", ({ game }) => {
-	game.currentScene.setFlash = () => {}; // who tf changed this
+	game.currentScene.setFlash = () => {};
+	game.currentScene.toggleFlash = () => {};
 	game.currentScene.terrainManager.shadow.setShadowSize(1000000);
 	game.currentScene.terrainManager.shadow.setShadowSize = () => {};
 });
