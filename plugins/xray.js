5ac311e8ee0439f31e2f199acff11145acc0aced
ItsPi3141 <90981829+ItsPi3141@users.noreply.github.com>
2026-05-08 02:12:30 -0700

    fix xray when respawning

--- plugins/xray.js
+++ plugins/xray.js
@@ -11,12 +11,17 @@ setInterval(() => {
 		game.currentScene.ceilingsContainer.alpha = 0.3;
 	} catch {}
 }, 200);
+setInterval(() => {
+	try {
+		if (game == null || game.currentScene == null) return;
+		game.currentScene.foodGlowContainer.zOrder = 996;
+		game.currentScene.foodContainer.zOrder = 997;
+		game.currentScene.namesLayer.zOrder = 998;
+		game.currentScene.animalsContainer.zOrder = 999;
+		game.currentScene.barsLayer.zOrder = 1000;
+		game.currentScene.chatContainer.zOrder = 1001;
+	} catch {}
+}, 5000);
 blockyfish.addEventListener("gameInit", ({ game: _game }) => {
 	game = _game;
-	game.currentScene.foodGlowContainer.zOrder = 996;
-	game.currentScene.foodContainer.zOrder = 997;
-	game.currentScene.namesLayer.zOrder = 998;
-	game.currentScene.animalsContainer.zOrder = 999;
-	game.currentScene.barsLayer.zOrder = 1000;
-	game.currentScene.chatContainer.zOrder = 1001;
 });
